8f1b97d3ee0ff9f5c766568148290ecc88d5a9ea
Jonathan_Yeshayahu <73723174+JonathanYesh279@users.noreply.github.com>
2025-04-18 16:14:10 +0300

    add location feild to orchestra validation file

--- api/orchestra/orchestra.validation.js
+++ api/orchestra/orchestra.validation.js
@@ -2,6 +2,40 @@ import Joi from 'joi'
 
 const VALID_NAMES = ['תזמורת מתחילים נשיפה', 'תזמורת עתודה נשיפה', 'תזמורת צעירה נשיפה', 'תזמורת יצוגית נשיפה', 'תזמורת סימפונית']
 const VALID_TYPES = ['הרכב', 'תזמורת']
+const VALID_LOCATIONS = [
+  'אולם ערן',
+  'סטודיו קאמרי 1',
+  'סטודיו קאמרי 2',
+  'אולפן הקלטות',
+  'חדר חזרות 1',
+  'חדר חזרות 2',
+  'חדר מחשבים',
+  'חדר 1',
+  'חדר 2',
+  'חדר חזרות',
+  'חדר 5',
+  'חדר 6',
+  'חדר 7',
+  'חדר 8',
+  'חדר 9',
+  'חדר 10',
+  'חדר 11',
+  'חדר 12',
+  'חדר 13',
+  'חדר 14',
+  'חדר 15',
+  'חדר 16',
+  'חדר 17',
+  'חדר 18',
+  'חדר 19',
+  'חדר 20',
+  'חדר 21',
+  'חדר 22',
+  'חדר 23',
+  'חדר 24',
+  'חדר 25',
+  'חדר 26',
+];
 
 export const orchestraSchema = Joi.object({
   name: Joi.string().valid(...VALID_NAMES).required(),
@@ -10,6 +44,7 @@ export const orchestraSchema = Joi.object({
   memberIds: Joi.array().items(Joi.string()).default([]),
   rehearsalIds: Joi.array().items(Joi.string()).default([]),
   schoolYearId: Joi.string().required(),
+  location: Joi.string().valid(...VALID_LOCATIONS).default('חדר 1'),
   isActive: Joi.boolean().default(true)
 })
 
@@ -19,5 +54,6 @@ export function validateOrchestra(orchestra) {
 
 export const ORCHESTRA_CONSTANTS = {
   VALID_TYPES,
-  VALID_NAMES
-}
+  VALID_NAMES,
+  VALID_LOCATIONS
+};
